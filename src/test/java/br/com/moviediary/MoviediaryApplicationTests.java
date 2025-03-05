f5ab75392a27f3b74a127127450fd5273532fcd7
Leandro R. <36233004+ReaperCord@users.noreply.github.com>
2025-03-05 20:59:58 -0300

    Initial Commit: Cria o projeto base.

---
+++ src/test/java/br/com/moviediary/MoviediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moviediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
